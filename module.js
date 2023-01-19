26e4747f231d418803dfd9885b5f061c4d0870f5
SouhilRM <r.m.souhil47@gmail.com>
2023-01-19 11:45:35 +0100

    les modules

---
+++ module.js
@@ -0,0 +1,3 @@
+//une fonction qui calcule la somme d'un tableau
+export const sum = (tableau) => tableau.reduce((acc, item) =>acc + item, 0) //n'oublie pas le export
+export const hello = ()=> console.log("Hola amigos !!!")
